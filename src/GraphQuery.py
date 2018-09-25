5c140c163a33d422a50905b8f574480300f415ec
Lituta <lituta@outlook.com>
2018-09-24 23:38:56 -0700

    update graph response format

--- src/GraphQuery.py
+++ src/GraphQuery.py
@@ -5,7 +5,7 @@ from src.SingleGraphQuery import SingleGraphQuery
 
 class GraphQuery(object):
     def __init__(self, xml_file_or_string):
-        self.roots = []
+        self.root = ET.Element('graphqueries_responses')
         self.query_list = xml_loader(xml_file_or_string, GRAPH_QUERY)
 
     def ask_all(self, endpoint, start=0, end=None, root_doc=''):
@@ -15,7 +15,7 @@ class GraphQuery(object):
             target_docs = set([c2p[c_id] for c_id in list(find_keys(DOCEID, self.query_list[i][ENTRYPOINTS]))])
             if root_doc in target_docs:
                 responses = self.ans_one(endpoint, self.query_list[i], root_doc)
-                self.roots.append(responses)
+                self.root.append(responses)
 
     def ans_one(self, endpoint, q_dict, root_doc):
         single = SingleGraphQuery(endpoint, q_dict, root_doc)
@@ -23,8 +23,8 @@ class GraphQuery(object):
         return responses
 
     def dump_responses(self, output_file):
-        if self.roots:
-            write_file(self.roots, output_file)
+        if len(self.root):
+            write_file(self.root, output_file)
             return True
         return False
 
