2139ab02e1dc552e62ffabbc2b5ce68d0e350b06
Lituta <dongyul@gaiadev01.isi.edu>
2018-10-02 18:52:46 -0700

    tmp

--- src/tools/mapping_name.sh
+++ src/tools/mapping_name.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for f in /Users/dongyuli/isi/data/dis_r0nl/dir_0/*;
+for f in /nas/home/dongyul/gaia_ta1/0923r1aug/*;
 do
     echo $f >> tmp.txt ;
     perl -nle 'print $& if m{(?<=aida:hasName      ").+(?=")}' $f >> tmp.txt ;
